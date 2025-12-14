6a24a799e59e1923e613804fe1cd93063a742e67
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-12-14 17:35:35 +0530

    04: test: add comprehensive unit tests for AuthForm (SignIn/SignUp)

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,8 @@
 import "@testing-library/jest-dom";
+import { mockToast, mockUseRouter, mockUseToast } from "./tests/mocks";
 
-jest.mock("next/navigation", () => ({
-  useRouter: jest.fn(() => ({ replace: jest.fn(), push: jest.fn })),
+jest.mock("next/navigation", () => ({ useRouter: mockUseRouter }));
+jest.mock("@/hooks/use-toast", () => ({
+  useToast: mockUseToast,
+  toast: mockToast,
 }));
