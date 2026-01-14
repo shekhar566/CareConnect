7e04189e53be6fa7c27b3d75dbe54098d3d8b5f6
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2026-01-14 11:50:42 +0530

    06: Tested AnswerForm Submission

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,5 +1,7 @@
 export * from "./router.mock";
 export * from "./toast.mock";
+export * from "./editor.mock";
+export * from "./nextauth.mock";
 
 // prevents TEST POLLUTION
 export const resetAllMocks = () => {
